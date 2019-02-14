cc98f2ca72c5daea648dd48acfb3a5469132b56e
Vanessa Martins <contato.vanessamartins@hotmail.com>
2019-02-14 12:15:52 -0200

    Inserção dos arquivos

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('nav');
+
+menu.onclick = function() {
+
+nav.classList.toggle ('nav-visivel');
+
+
+}
+
